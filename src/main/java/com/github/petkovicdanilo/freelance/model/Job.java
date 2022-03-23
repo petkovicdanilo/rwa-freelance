1bd8b2829a745a2927b061a9ee7b94fdb2ddf387
Danilo Petkovic <petkovicdanilo97@gmail.com>
2022-03-23 19:56:37 +0100

    Add basic Hibernate support

--- src/main/java/com/github/petkovicdanilo/freelance/model/Job.java
+++ src/main/java/com/github/petkovicdanilo/freelance/model/Job.java
@@ -2,11 +2,21 @@ package com.github.petkovicdanilo.freelance.model;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @AllArgsConstructor
 @Data
-@ToString
 public class Job {
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(length = 1024)
     private String description;
+
     private double price;
 }
--- src/main/java/com/github/petkovicdanilo/freelance/model/Technology.java
+++ src/main/java/com/github/petkovicdanilo/freelance/model/Technology.java
@@ -3,9 +3,17 @@ package com.github.petkovicdanilo.freelance.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @AllArgsConstructor
 public class Technology {
+    @Id
+    @GeneratedValue
     private int id;
+
     private String name;
 }
