e55fe94d702083aedb246003ee83c64902f0af40
Danilo Petkovic <petkovicdanilo97@gmail.com>
2022-03-30 20:09:05 +0200

    Comment out db initializer

--- src/main/java/com/github/petkovicdanilo/freelance/stub/DbInitializer.java
+++ src/main/java/com/github/petkovicdanilo/freelance/stub/DbInitializer.java
@@ -26,52 +26,52 @@ public class DbInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user1 = User.builder()
-                .firstName("Pera")
-                .lastName("Peric")
-                .email("pera.peric@example.com")
-                .password("pera")
-                .gender(User.Gender.MALE)
-                .build();
-        User user2 = User.builder()
-                .firstName("Milica")
-                .lastName("Milicevic")
-                .email("milica@example.com")
-                .password("milica")
-                .gender(User.Gender.FEMALE)
-                .build();
-        User user3 = User.builder()
-                .firstName("Vanja")
-                .lastName("Vanjic")
-                .email("vanja@example.com")
-                .password("vanja")
-                .build();
-
-        usersRepository.saveAll(Arrays.asList(user1, user2, user3));
-        logger.info("Initialized users");
-
-        Job job1 = Job.builder()
-                .description("job 1")
-                .price(100)
-                .build();
-        Job job2 = Job.builder()
-                .description("job 2")
-                .price(200)
-                .build();
-        Job job3 = Job.builder()
-                .description("job 3")
-                .price(300)
-                .build();
-
-        jobsRepository.saveAll(Arrays.asList(job1, job2, job3));
-        logger.info("Initialized jobs");
-
-        Technology java = Technology.builder().name("Java").build();
-        Technology cSharp = Technology.builder().name("C#").build();
-        Technology spring = Technology.builder().name("Spring").build();
-        Technology cpp = Technology.builder().name("C++").build();
-
-        technologiesRepository.saveAll(Arrays.asList(java, cSharp, spring, cpp));
-        logger.info("Initialized technologies");
+//        User user1 = User.builder()
+//                .firstName("Pera")
+//                .lastName("Peric")
+//                .email("pera.peric@example.com")
+//                .password("pera")
+//                .gender(User.Gender.MALE)
+//                .build();
+//        User user2 = User.builder()
+//                .firstName("Milica")
+//                .lastName("Milicevic")
+//                .email("milica@example.com")
+//                .password("milica")
+//                .gender(User.Gender.FEMALE)
+//                .build();
+//        User user3 = User.builder()
+//                .firstName("Vanja")
+//                .lastName("Vanjic")
+//                .email("vanja@example.com")
+//                .password("vanja")
+//                .build();
+//
+//        usersRepository.saveAll(Arrays.asList(user1, user2, user3));
+//        logger.info("Initialized users");
+//
+//        Job job1 = Job.builder()
+//                .description("job 1")
+//                .price(100)
+//                .build();
+//        Job job2 = Job.builder()
+//                .description("job 2")
+//                .price(200)
+//                .build();
+//        Job job3 = Job.builder()
+//                .description("job 3")
+//                .price(300)
+//                .build();
+//
+//        jobsRepository.saveAll(Arrays.asList(job1, job2, job3));
+//        logger.info("Initialized jobs");
+//
+//        Technology java = Technology.builder().name("Java").build();
+//        Technology cSharp = Technology.builder().name("C#").build();
+//        Technology spring = Technology.builder().name("Spring").build();
+//        Technology cpp = Technology.builder().name("C++").build();
+//
+//        technologiesRepository.saveAll(Arrays.asList(java, cSharp, spring, cpp));
+//        logger.info("Initialized technologies");
     }
 }
